b2aa54ba1a782d3e6773acc8fb99607c4eb0a4a0
UtakataKyosui <fill.ayaextech@gmail.com>
2025-11-19 14:07:58 +0900

    feat: deriveマクロをコードを書いて理解した

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use hello::HelloMacro;
+use hello_derive::HelloMacro;
+   
+struct Pancakes;
+
+// impl HelloMacro for Pancakes {
+// 	fn hello_macro() {
+// 		println!("Hello, Macro! My name is Pancakes!");
+// 	}
+// }
+
+#[derive(HelloMacro)]
+struct HotCake;
+
 fn main() {
-    println!("Hello, world!");
+	// Pancakes::hello_macro();
+	HotCake::hello_macro();
 }
